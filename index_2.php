8cf56a5fb1f2c9d4f453d3fa3ea2e31a0415d271
Christian Podda <poddachristian94@gmail.com>
2022-05-05 20:08:14 +0200

    Update controllo parametri

--- index_2.php
+++ index_2.php
@@ -1,9 +1,16 @@
 <?php
 
 // Parametri
-
 $name = $_GET["name"];
 $mail = $_GET["mail"];
 $age = $_GET["age"];
+
+// Controllo parametri
+$mailAccess = preg_match('(.|@)', $mail);
+$ageCheck = is_numeric($age);
+$stringLen = strlen($name);
+
+
+// Stampa risposta parametri
 
 ?>
