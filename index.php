a61e46331f981baab17eb43849f71f8198468b53
Christian Podda <poddachristian94@gmail.com>
2022-05-05 19:09:34 +0200

    completed

--- index.php
+++ index.php
@@ -2,14 +2,30 @@
 
 //phpinfo();
 
+// Partite di calendario
 $games = [
     [
-        "team1" => "Olimpia Milano",
-        "team2" => "Cantù",
-        "team1_score" => 55,
-        "team2_score" => 60
+        "hostTeam" => "Colorado Moles",
+        "guestTeam" => "Texas Bulls",
+        "hostPoints"  => 53,
+        "guestPoints"  => 38,
     ],
+    [
+        "hostTeam" => "Coyote Kansas",
+        "guestTeam" => "Fireflies Chicago",
+        "hostPoints"  => 29,
+        "guestPoints"  => 47,
+    ],
+        [
+        "hostTeam" => "Alabama Kangaroo",
+        "guestTeam" => "San Francisco Animals",
+        "hostPoints"  => 18,
+        "guestPoints"  => 44,
+    ],
+];
 
-]
-
- ?>
+for ($i = 0; $i < count($games); $i++) {
+    $game = $games[$i];
+    $match = $game["hostTeam"] . " - " . $game["guestTeam"] . " | " . $game["hostPoints"] . "-" . $game["guestPoints"];
+    echo $match . " | ";
+}
